7a3887b0b4e81e231334f75ca601b080b463a137
acadevy <rashyolat@gmail.com>
2023-01-02 10:59:40 +0100

    console-practice

--- Program.cs
+++ Program.cs
@@ -6,7 +6,12 @@ namespace C__practice
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+          Console.WriteLine("Enter your first number");
+          int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter your second number");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            int sum = num1 + num2;
+            Console.WriteLine($"The sum is: {sum}");
         }
     }
 }
